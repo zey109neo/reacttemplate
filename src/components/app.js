3309d759d6ec16e85801dc886f0cc5768b5f2c3f
Ezekiel <ezekielfrankford@gmail.com>
2022-11-11 16:45:36 -0600

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>My Test Application</h1>
+        <h2>React Bootstrap</h2>
+      </div>
+    );
+  }
+}
